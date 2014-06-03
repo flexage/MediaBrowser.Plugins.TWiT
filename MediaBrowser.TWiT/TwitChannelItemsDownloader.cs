067b1c5ccf18f8f49315d7cab20a987d8d5f43e5
Stephen Maunder <snazy2000@btinternet.com>
2014-06-04 00:06:01 +0100

    Resharper Optimizations :)
    Removes un needed items. Shortened code to make plugin size smaller and plugin faster to run

--- MediaBrowser.TWiT/TwitChannelItemsDownloader.cs
+++ MediaBrowser.TWiT/TwitChannelItemsDownloader.cs
@@ -9,7 +9,6 @@ namespace MediaBrowser.Plugins.TWiT
 {
     public class TwitChannelItemsDownloader
     {
-
         private ILogger _logger;
         private readonly IHttpClient _httpClient;
         private readonly IXmlSerializer _xmlSerializer;
@@ -23,14 +22,10 @@ namespace MediaBrowser.Plugins.TWiT
 
         public async Task<rss> GetStreamList(String queryUrl, int offset, CancellationToken cancellationToken)
         {
-            rss feed;
-
             using (var xml = await _httpClient.Get(queryUrl, CancellationToken.None).ConfigureAwait(false))
             {
-                feed = _xmlSerializer.DeserializeFromStream(typeof(rss), xml) as rss;
+                return _xmlSerializer.DeserializeFromStream(typeof(rss), xml) as rss;
             }
-
-            return feed;
         }
 
     }
